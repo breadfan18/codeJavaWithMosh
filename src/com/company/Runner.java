bb00adbe59eb27d7557b9a4a807c547f6d5603ac
Swaroop Uprety <breadfan18@gmail.com>
2020-10-13 15:56:04 -0400

    ternary operator

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println(isFirst(110_000));
+    }
+
+    public static String isFirst(int income) {
+        return (income > 100_000) ? "First" : "Economy";
+    }
 }
