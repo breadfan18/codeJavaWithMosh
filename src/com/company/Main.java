af6723973339ed6a885f95d551fb991bb52ec634
Swaroop Uprety <breadfan18@gmail.com>
2020-10-14 17:47:57 -0400

    oct 14 commit 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,33 +14,11 @@ public class Main {
         final byte MONTHS_IN_YEAR = 12;
         final byte PERCENT = 100;
 
-        int principal, numberOfYears, numberOfMonths = 0;
-        double yearlyRate, monthlyRate = 0;
-
-        while (true) {
-            principal =  helper.getIntFromScanner("Principal ($1K - $1M): ");
-            if (principal >= 1000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a number between 1,000 and 1,000,000");
-        }
-
-        while (true) {
-            yearlyRate = helper.getDoubleFromScanner("Annual Interest Rate: ");
-            if (yearlyRate > 0 || yearlyRate <= 30){
-                monthlyRate = (yearlyRate/PERCENT)/MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a number greater than 0 and less than or equal to 30");
-        }
-
-        while (true) {
-            numberOfYears = helper.getIntFromScanner("Period (\"Years\"): ");
-            if (numberOfYears >= 1 && numberOfYears <= 30) {
-                numberOfMonths = numberOfYears * MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
+        int principal = (int) readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
+        double yearlyRate = readNumber("Annual Interest Rate: ", 0, 30);
+        double monthlyRate = (yearlyRate/PERCENT)/MONTHS_IN_YEAR;
+        int numberOfYears = (int) readNumber("Period (\"Years\"): ", 1, 30);
+        int numberOfMonths = numberOfYears * MONTHS_IN_YEAR;
 
         System.out.println("-----------------------------------");
         System.out.println("Mortgage: " + mortgageFormula(principal, monthlyRate, numberOfMonths));
@@ -54,9 +32,18 @@ public class Main {
 
         monthlyPayment = principal * (formulaNumerator/formulaDenominator);
 
-        String formattedMonthlyPayment = NumberFormat.getCurrencyInstance().format(monthlyPayment);
-        return formattedMonthlyPayment;
+        return NumberFormat.getCurrencyInstance().format(monthlyPayment);
     }
 
-    public static doube validation (String userInput, String doubleOrInt, )
+    public static double readNumber(String prompt, double min, double max) {
+        double value;
+
+        while (true) {
+            value =  helper.getDoubleFromScanner(prompt);
+            if (value > min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
 }
